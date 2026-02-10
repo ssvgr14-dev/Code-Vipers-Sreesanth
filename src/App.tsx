cea4916ed21b28232186f0afac80d7244bda7736
ssvgr14-dev <ssvgr14@gmail.com>
2026-02-10 13:12:39 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "@/contexts/AppContext";
+import Layout from "@/components/Layout";
+import Index from "./pages/Index";
+import Marketplace from "./pages/Marketplace";
+import FarmerDashboard from "./pages/FarmerDashboard";
+import AdminPanel from "./pages/AdminPanel";
+import Checkout from "./pages/Checkout";
+import OrderConfirmation from "./pages/OrderConfirmation";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <AppProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/marketplace" element={<Marketplace />} />
+              <Route path="/farmer" element={<FarmerDashboard />} />
+              <Route path="/admin" element={<AdminPanel />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/order-confirmation" element={<OrderConfirmation />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </AppProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
